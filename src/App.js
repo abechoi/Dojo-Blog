da63be39232bdf7c222520c8fae3b49f5c44eb21
Abraham Choi <abraham.choi@icloud.com>
2021-04-17 21:36:12 -0700

    Mapping List

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import Navbar from "./Navbar";
 import Home from "./Home";
-import { useState } from "react";
 
 function App() {
-  const [name, setName] = useState("Mario");
-  const [age, setAge] = useState(25);
-
-  const handleClick = () => {
-    setName("Luigi");
-    setAge(30);
-  };
   return (
     <div className="App">
       <Navbar />
       <div className="content">
         <Home />
-        <p>
-          {name} is {age} years old.
-        </p>
-        <button onClick={handleClick}>Click Me</button>
       </div>
     </div>
   );
--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,26 @@
+import { useState } from "react";
+
 const Home = () => {
-  return <div className="home">Homepage</div>;
+  const [blogs, setBlogs] = useState([
+    { title: "My new website", body: "lorem ipsum...", author: "Mario", id: 1 },
+    { title: "Welcome Party", body: "lorem ipsum...", author: "yoshi", id: 2 },
+    {
+      title: "Web dev top tips!",
+      body: "lorem ipsum...",
+      author: "Mario",
+      id: 3,
+    },
+  ]);
+  return (
+    <div className="home">
+      {blogs.map((blog) => (
+        <div className="blog-prev" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Home;

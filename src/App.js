ce20ef65bcdb6842fbeea1a07950e58532d089c5
Abraham Choi <abraham.choi@icloud.com>
2021-04-17 21:15:24 -0700

    Using State

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import Navbar from "./Navbar";
 import Home from "./Home";
+import { useState } from "react";
 
 function App() {
-  const handleClick = (name, e) => {
-    console.log("Hello " + name, e.target);
+  const [name, setName] = useState("Mario");
+  const [age, setAge] = useState(25);
+
+  const handleClick = () => {
+    setName("Luigi");
+    setAge(30);
   };
   return (
     <div className="App">
       <Navbar />
       <div className="content">
         <Home />
-        <button onClick={(e) => handleClick("Mario", e)}>Click Me</button>
+        <p>
+          {name} is {age} years old.
+        </p>
+        <button onClick={handleClick}>Click Me</button>
       </div>
     </div>
   );

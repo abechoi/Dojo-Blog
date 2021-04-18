80f3bb616e4ff2bf489cafcd991ca0ee34208977
Abraham Choi <abraham.choi@icloud.com>
2021-04-17 21:04:12 -0700

    Click Events

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import Navbar from "./Navbar";
 import Home from "./Home";
 
 function App() {
+  const handleClick = (name, e) => {
+    console.log("Hello " + name, e.target);
+  };
   return (
     <div className="App">
       <Navbar />
       <div className="content">
         <Home />
+        <button onClick={(e) => handleClick("Mario", e)}>Click Me</button>
       </div>
     </div>
   );

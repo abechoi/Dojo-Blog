867d270b69b24d31728edb33003f29dc649a1cc4
Abraham Choi <abraham.choi@icloud.com>
2021-04-18 23:14:14 -0700

    Functions as props

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,10 +1,11 @@
-const BlogList = ({ blogs }) => {
+const BlogList = ({ blogs, handleDelete }) => {
   return (
     <div className="blog-list">
       {blogs.map((blog) => (
         <div className="blog-prev" key={blog.id}>
           <h2>{blog.title}</h2>
           <p>Written by {blog.author}</p>
+          <button onClick={() => handleDelete(blog.id)}>delete</button>
         </div>
       ))}
     </div>
--- src/Home.js
+++ src/Home.js
@@ -12,9 +12,15 @@ const Home = () => {
       id: 3,
     },
   ]);
+
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter((blog) => blog.id !== id);
+    setBlogs(newBlogs);
+  };
+
   return (
     <div className="home">
-      <BlogList blogs={blogs} />
+      <BlogList blogs={blogs} handleDelete={handleDelete} />
     </div>
   );
 };
